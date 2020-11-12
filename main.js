09bbaad565d2b98af86a96e89d1f6aeaca8240ab
ZOmohundro <zaneomohundro@gmail.com>
2020-11-12 17:49:41 -0500

    initial grid and start of javascript

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+//cache main container div
+
+const container = document.getElementById('container');
+
+
+
+//create 12 divs and attach them to #container
+
+for (let i = 0;i < 256;i++){
+    const div = document.createElement('div');
+    div.innerText = i
+    div.className = "gridItem"
+    container.appendChild(div);
+}
+
+//cache the newly created gridItems
+
+const gridItem_div = document.getElementsByClassName('gridItem');
+console.log(typeof(gridItem_div));
+
+//add event listener to created div elements applies class divChanged
+
+ gridItem_div.forEach(key => key.addEventListener('mouseenter', (e) => {
+     console.log(e.target);
+ }));
+
+//test
+
