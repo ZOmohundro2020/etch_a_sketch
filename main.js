1822d2fa677ea7f314245d7c27932c8e9fe107a5
ZOmohundro <zaneomohundro@gmail.com>
2020-11-13 14:45:50 -0500

    Establish initial functionality and add reset button

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-//cache main container div
+//cache main container div and button
 
 const container = document.getElementById('container');
+const resetButton = document.getElementById('resetButton');
 
+//set initial grid size
+let gridSize = 16;
 
 
 //create 12 divs and attach them to #container
 
-for (let i = 0;i < 256;i++){
+for (let i = 0;i < gridSize**2;i++){
     const div = document.createElement('div');
-    //div.innerText = i;
+    div.innerText = i;
     div.className = "gridItem";
     container.appendChild(div);
 }
@@ -16,16 +19,37 @@ for (let i = 0;i < 256;i++){
 //cache the newly created gridItems
 
 const gridItem_div = document.getElementsByClassName('gridItem');
-console.log(typeof(gridItem_div));
 
 //add event listener to created div elements applies class divChanged
 
 Array.from(gridItem_div).forEach(function (e){
     e.addEventListener('mouseenter', function (e) {
         //console.log(e.target);
-        this.className = "divChanged"
+        this.classList.add("divChanged");
     });
 });
+
+//add event listener for button to reset the board
+
+// original anon function
+// resetButton.addEventListener('click', () => {
+//     console.log("button clicked");
+// });
+
+//with named function instead
+
+resetButton.addEventListener('click', buttonClicked);
+
+//when resetButton is clicked, remove class divChanged
+
+function buttonClicked() {
+    Array.from(gridItem_div).forEach(function (e){
+        e.classList.remove("divChanged");
+    });
+
+}
+
+
 
 //test
 // this.gridButtonItems.forEach(function (item, idx) {
