7ba14209a5c4e8cf960c9f6635d0cc3e225c70eb
ZOmohundro <zaneomohundro@gmail.com>
2020-11-13 22:26:19 -0500

    clarify initial functionality. get reset button working.

--- main.js
+++ main.js
@@ -1,59 +1,80 @@
-//cache main container div and button
+//cache main items
 
 const container = document.getElementById('container');
 const resetButton = document.getElementById('resetButton');
-
-//set initial grid size
-let gridSize = 16;
-
-
-//create 12 divs and attach them to #container
-
-for (let i = 0;i < gridSize**2;i++){
-    const div = document.createElement('div');
-    div.innerText = i;
-    div.className = "gridItem";
-    container.appendChild(div);
-}
-
-//cache the newly created gridItems
-
 const gridItem_div = document.getElementsByClassName('gridItem');
 
-//add event listener to created div elements applies class divChanged
 
-Array.from(gridItem_div).forEach(function (e){
-    e.addEventListener('mouseenter', function (e) {
-        //console.log(e.target);
-        this.classList.add("divChanged");
+
+//set initial grid size
+var gridSize = 16;
+
+
+//set the CSS grid based on gridSize
+
+function setGrid(gridSize){
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+}
+
+
+
+
+//create ${gridSize} divs and attach them to #container
+function makeDivs(){
+    
+    clearDivs();
+    for (let i = 0;i < gridSize**2;i++){
+        const div = document.createElement('div');
+        //div.innerText = i; //debug to see numbers in divs
+        div.className = "gridItem";
+        container.appendChild(div);
+    }
+    addListeners();
+}
+
+
+
+//add event listener to created div elements that applies class divChanged when mouse enters div
+function addListeners(){
+    
+
+    Array.from(gridItem_div).forEach(function (e){
+        e.addEventListener('mouseenter', function (e) {
+            //console.log(e.target);
+            this.classList.add("divChanged");
+        });
     });
-});
+}
+
+
 
 //add event listener for button to reset the board
-
-// original anon function
-// resetButton.addEventListener('click', () => {
-//     console.log("button clicked");
-// });
-
-//with named function instead
 
 resetButton.addEventListener('click', buttonClicked);
 
 //when resetButton is clicked, remove class divChanged
+//then get a new gridSize from the user
+//then start the main loop by making divs -- makeDivs()
 
 function buttonClicked() {
     Array.from(gridItem_div).forEach(function (e){
         e.classList.remove("divChanged");
     });
+    let getGridSize = prompt("Please enter a new grid size (1-100)");
+    gridSize = getGridSize;
 
+    setGrid(gridSize);
+    makeDivs();
 }
 
+//function that removes all divs to clear thigns out before changing sizes
 
+function clearDivs(){
+    container.innerHTML = '';
+}
 
-//test
-// this.gridButtonItems.forEach(function (item, idx) {
-//     item.addEventListener('click', function () {
-//         self._showContent(idx);
-//     });
-// });
+//main initlization
+setGrid(gridSize);
+makeDivs();
+addListeners();
